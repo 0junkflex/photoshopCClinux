732683ec3e1419f0eccb755636c25ae272f34b9c
Junkflex <junkflex@proton.me>
2025-02-20 12:52:50 +1100

    Update PhotoshopSetup.sh

--- scripts/PhotoshopSetup.sh
+++ scripts/PhotoshopSetup.sh
@@ -96,7 +96,7 @@ function replacement() {
 function install_photoshopSE() {
     local filename="photoshopCC-V19.1.6-2018x64.tgz"
     local filemd5="b63f6ed690343ee12b6195424f94c33f"
-    local filelink="https://junkflex.com/photoshopCC-V19.1.6-2018x64.tgz"
+    local filelink="file://$HOME/Downloads/photoshopCC-V19.1.6-2018x64.tgz"
     # local filelink="http://127.0.0.1:8080/photoshopCC-V19.1.6-2018x64.tgz"
     local filepath="$CACHE_PATH/$filename"
 
